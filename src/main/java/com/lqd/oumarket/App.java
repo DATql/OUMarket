9b226763b1bd7f699415870c87b9f3cedd729c3b
phamtanhoang <88889770+phamtanhoang@users.noreply.github.com>
2023-04-09 16:30:06 +0700

    DONE interface

--- src/main/java/com/lqd/oumarket/App.java
+++ src/main/java/com/lqd/oumarket/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("Login"));
+        scene = new Scene(loadFXML("BranchUI"));
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.lqd.oumarket {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-
+    requires de.jensd.fx.glyphs.fontawesome;
     opens com.lqd.oumarket to javafx.fxml;
     exports com.lqd.oumarket;
     exports com.lqd.pojo;
